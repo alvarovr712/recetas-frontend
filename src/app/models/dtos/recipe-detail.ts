562eaac7e3cdf236c29da777b4f4c4d7a0f45fe0
alvarovr712 <alvarovr712@gmail.com>
2026-03-10 10:54:49 +0100

    mapeado endpoint getRecipeDetail() en recipe service

---
+++ src/app/models/dtos/recipe-detail.ts
@@ -0,0 +1,26 @@
+export interface RecipeDetailDto {
+  id: string;
+  image: string;
+  title: string;
+  type: string;
+  prepTime: number;
+  servings: number;
+  description: string;
+
+  userName: string;
+  userImage: string;
+
+  ingredients: RecipeIngredientDto[];
+  steps: RecipeStepDto[];
+}
+
+export interface RecipeIngredientDto {
+  quantity: string;
+  unit: string;
+}
+
+export interface RecipeStepDto {
+  stepOrder: number;
+  instruction: string;
+  image: string;
+}
--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -5,6 +5,7 @@ import { tap } from 'rxjs/operators';
 import { Ingredient } from '../models/ingredient.model';
 import { CreateRecipeRequest } from '../models/dtos/create-recipe-request';
 import { RecipeCard } from '../models/dtos/recipe-card';
+import { RecipeDetailDto } from '../models/dtos/recipe-detail';
 
 
 
@@ -23,6 +24,13 @@ export class RecipeService {
     getMisRecetas(): Observable<RecipeCard[]>{
         return this.http.get<RecipeCard[]>(`${this.baseUrl}/mis-recetas`,{withCredentials:true});
     }
+
+    getRecipeDetail(id: string): Observable<RecipeDetailDto> {
+    return this.http.get<RecipeDetailDto>(
+      `${this.baseUrl}/detalle/${id}`,
+      { withCredentials: true }
+    );
+  }
 
    
 }
